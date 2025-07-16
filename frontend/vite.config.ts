c5da591ce36bd25f164f78545ce77213c2be685f
4dsjunior <adsjunior@me.com>
2025-07-16 14:11:28 -0300

    90% pronto v2.0

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+//import { defineConfig } from 'vite';
+//import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+// optimizeDeps: {
+//    exclude: ['lucide-react'],
+//  },
+//});
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +19,8 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  server: {
+    host: true, // Permite acesso externo
+    allowedHosts: ['.ngrok-free.app'], // Permite qualquer subdomínio do ngrok
+  },
+})
